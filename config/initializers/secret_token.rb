c171be36d1790ca89c8b81dca8d9b43900f59622
Maximilian Wittmann <damxam@gmail.com>
2014-01-01 15:38:27 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecv::Application.config.secret_key_base = 'dffcfa1b56767e8e36a785376e0b78738c781b2c1d1207d126bacdb037b2461ef3566009829994d11f295d2516c3238c7c3135572312d60e0a0a88617cb96580'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  subject {page}
+
+  describe "should have header" do
+    before { visit root_path }
+    it {should have_selector(:id, 'header')}
+  end
+
+  describe "should have footer" do
+    before { visit root_path }
+    it {should have_selector(:id, 'footer')}
+  end
+
+  describe "main page" do
+    before { visit '/' }
+
+    describe "should have default name (by default)" do
+    it {should have_content("Default Name")}
+    end
+
+  end
+
+end
